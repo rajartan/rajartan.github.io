f67f278c867eb0fa316b1729f65e2b05add3670e
Rachel Ardana Putra Ginting <rachelardanaputraginting@gmail.com>
2022-08-13 10:07:54 +0700

    "Menghapus dashboard admin dan file yang terkait"

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AdminTutorialController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TutorialController;
 use Illuminate\Support\Facades\Route;
@@ -24,5 +23,4 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/tutorial', [TutorialController::class, 'index']);
 
 // Admin Dashboard
-Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/dashboard/tutorial', [AdminTutorialController::class, 'index']);
