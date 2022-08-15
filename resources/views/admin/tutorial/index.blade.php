fbb9ad461e2d5cbd174b700d322d9cfbd2d20a50
Rachel Ardana Putra Ginting <rachelardanaputraginting@gmail.com>
2022-08-15 10:53:49 +0700

    Customize pagination tailwindcss

--- resources/views/admin/tutorial/index.blade.php
+++ resources/views/admin/tutorial/index.blade.php
@@ -37,7 +37,7 @@
                                 @foreach ($tutorials as $tutorial)
                                 <tr class="bg-white dark:bg-gray-800">
                                     <td class="py-4 px-6">
-                                        {{ $loop->iteration }}
+                                        {{ $loop->index + $tutorials->firstItem() }}
                                     </td>
                                     <td class="py-4 px-6">
                                         {{ $tutorial->image }}
@@ -62,18 +62,9 @@
                             </tbody>
                         </table>
 
-                        <nav aria-label="Page navigation example">
-                            <ul class="inline-flex -space-x-px">
-                            <li>
-                                <a href="#" class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{ $loop->iteration }}</a>
-                            <li>
-                                <a href="#" class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
-                            </li>
-                            </ul>
-                        </nav>
+                        <div class="px-4 mt-4">
+                            {{ $tutorials->links('vendor.pagination.tailwind') }}
+                        </div>
 
                     </div>
                 </div>
